9e566031f928c8d523399eaeef1b8c20774b019b
Monica Lent <lent.monica@gmail.com>
2016-02-26 16:22:18 +0100

    Remove extra inclusion of zone and reflect, create readme.md.

--- client/universal-bootstrap.js
+++ client/universal-bootstrap.js
@@ -1,6 +1,3 @@
-import 'reflect-metadata';
-import 'zone.js';
-
 import { bootstrap } from 'angular2-universal-preview';
 import { ROUTER_PROVIDERS } from 'angular2/router';
 import { App } from 'universal-app';
